eba49503a175135e61c190f8a17847181e5606f7
danieldjordan@gmail.com <danieldjordan@gmail.com>
2018-08-10 21:21:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,3 +21,11 @@ function prependKitten (name) {
 function appendKitten (name) {
   return [...kittens, name]
 }
+
+function removeLastKitten(){
+  kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten(){
+  kittens.slice(1)
+}
