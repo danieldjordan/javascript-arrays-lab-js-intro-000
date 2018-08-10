f7897938f8f8b1809e81d4f1646441b034c679e4
danieldjordan@gmail.com <danieldjordan@gmail.com>
2018-08-10 21:17:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}// Add your functions and code here
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten (name) {
+  return kittens.splice(0,0, name)
+}
